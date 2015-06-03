593f3cee7fd439bcff0c62be080292d1d3e720f4
Tim <chent1101@gmail.com>
2015-06-03 09:48:37 -0600

    First commit, with initial blog posts

---
+++ backend/blog/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.template import Context, loader
+
+from models import Post
+
+
+def index(request):
+    latest_posts = Post.objects.all().order_by('-created_at')
+    t = loader.get_template('index.html')
+    c = Context({'latest_posts': latest_posts})
+    return HttpResponse(t.render(c))
+
+
+
