1bf650a78be0af926d3c5b7984cde5f5c9566af6
Tim <chent1101@gmail.com>
2015-06-04 15:28:13 -0600

    fixed load issue in views.py

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -4,5 +4,6 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', 'blog.views.index', name='index'),
+    url(r'^add_post/', views.add_post, name='add_post'),
     url(r'^(?P<post_url>\w+)/$', views.post, name='post'),
 )
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,18 +1,29 @@
 from django.http import HttpResponse
-from django.template import Context, loader
-from django.shortcuts import get_object_or_404
+from django.template import Context, loader, RequestContext
+from django.shortcuts import get_object_or_404, render_to_response, redirect
+
 
 from models import Post
+from forms import PostForm
+
+
+def encode_url(url):
+    return url.replace(' ', '_')
+
 
 
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
+    popular_posts = Post.objects.order_by('-views')[:5]
     t = loader.get_template('index.html')
-    context_dict = {'latest_posts': latest_posts, }
+    context_dict = {'latest_posts': latest_posts, 'popular_posts': popular_posts, }
     for post in latest_posts:
-        post.url = post.title.replace(' ', '_')
+        post.url = encode_url(post.title)
+    for popular_post in popular_posts:
+        popular_post.url = popular_post.title.replace(' ', '_')
     c = Context(context_dict)
     return HttpResponse(t.render(c))
+
 
 def post(request, post_url):
     single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
@@ -21,6 +32,20 @@ def post(request, post_url):
     t = loader.get_template('post.html')
     c = Context({'single_post': single_post, })
     return HttpResponse(t.render(c))
+
+def add_post(request):
+    context = RequestContext(request)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect(index)
+        else:
+            print form.errors
+    else:
+        form = PostForm()
+    return render_to_response('add_post.html', {'form': form}, context)
+
 
 
 
