aeb5e55309762438296056dc2c6c03f66fcf57cf
Tim <chent1101@gmail.com>
2015-06-04 19:41:09 -0600

    Fixed 500 issue on post view.

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -10,27 +10,32 @@ from forms import PostForm
 def encode_url(url):
     return url.replace(' ', '_')
 
+def get_popular_posts():
+    popular_posts = Post.objects.order_by('-views')[:5]
+    return popular_posts
 
 
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
-    popular_posts = Post.objects.order_by('-views')[:5]
     t = loader.get_template('index.html')
-    context_dict = {'latest_posts': latest_posts, 'popular_posts': popular_posts, }
-    for post in latest_posts:
-        post.url = encode_url(post.title)
-    for popular_post in popular_posts:
-        popular_post.url = popular_post.title.replace(' ', '_')
+    context_dict = {
+        'latest_posts': latest_posts,
+        'popular_posts': get_popular_posts(),
+    }
     c = Context(context_dict)
     return HttpResponse(t.render(c))
 
 
-def post(request, post_url):
-    single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
+def post(request, slug):
+    single_post = get_object_or_404(Post, slug=slug)
     single_post.views += 1 #increment the number of views per visit
     single_post.save()
     t = loader.get_template('post.html')
-    c = Context({'single_post': single_post, })
+    context_dict = {
+        'single_post': single_post,
+        'popular_posts': get_popular_posts(),
+    }
+    c = Context(context_dict)
     return HttpResponse(t.render(c))
 
 def add_post(request):
